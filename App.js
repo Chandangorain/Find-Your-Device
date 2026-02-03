e595a34ceb6e4acd6f82222b798acdadff7a5251
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-02-04 01:14:11 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const http=require('http')
+
+const socketio=require('socket.io');
+
+const server=http.createServer(app)
+const io=socketio(server)
+
+app.get('/', (req, res) => {
+  res.send('Hello !')
+})
+
+app.listen(port);
