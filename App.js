426e223397aad810dfc326791603c7e8fd95dfb1
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-02-04 01:52:15 +0530

    minjs mincss socketio cdn setup

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 const http=require('http')
 
 const socketio=require('socket.io');
-
 const server=http.createServer(app)
 const io=socketio(server)
+
+app.set('view engine','ejs')
+//app.set(express.static(path.join(__dirname,'public')))
 
 app.get('/', (req, res) => {
   res.send('Hello !')
 })
 
-app.listen(port);
+server.listen(port);
